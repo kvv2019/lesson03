853830d6e133eaaf9630bafd847c5dc2a83f9617
kovalchuk_vv <kovalchuk_vv@magnit.ru>
2021-12-04 03:00:21 +0300

    небольшой рефакторинг

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,6 +13,10 @@ def rnd_value():
     return (lambda x: 0.1 if x == 0 else x)(random.random())
 
 
+def calc_param3(param1, param2):
+    return math.sqrt(param1 ** 2 + param2 ** 2 - 2 * param1 * param2 * math.cos(random.randint(1, 179)))
+
+
 @pytest.fixture
 def figure_reg(test_figure, test_param1, test_param2, test_param3):
     if issubclass(test_figure, Circle) or issubclass(test_figure, Square):
@@ -32,7 +36,7 @@ def figure_rnd(test_figure):
     elif issubclass(test_figure, Triangle):
         param1 = rnd_value()
         param2 = rnd_value()
-        param3 = math.sqrt(param1 ** 2 + param2 ** 2 - 2 * param1 * param2 * math.cos(random.randint(1, 179)))
+        param3 = calc_param3(param1, param2)
         return test_figure(param1, param2, param3)
 
 
@@ -55,5 +59,5 @@ def rectangle_rnd():
 def triangle_rnd():
     param1 = rnd_value()
     param2 = rnd_value()
-    param3 = math.sqrt(param1 ** 2 + param2 ** 2 - 2 * param1 * param2 * math.cos(random.randint(1, 179)))
+    param3 = calc_param3(param1, param2)
     return Triangle(param1, param2, param3)
